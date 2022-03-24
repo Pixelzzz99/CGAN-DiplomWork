ce246ab4c0d83f323f2fa9616d7a2b46b78535b5
Sherzik <sherzik99@gmail.com>
2022-03-24 17:40:01 +0500

    Fixes JAN

--- losses.py
+++ losses.py
@@ -12,7 +12,7 @@ def custom_loss(y_true, y_pred):
 def custom_loss_2(y_true, y_pred):
     # scaled cosine_distance with MES + MAE
     cosine = losses.cosine_proximity(y_true, y_pred)
-    mse = losses.mean_squared_error(y_true, y_pred)
+    mse = losses.man_squared_error(y_true, y_pred)
     mle = losses.mean_absolute_error(y_true, y_pred)
     loss = (1 + cosine) * mse + mle
     return loss
