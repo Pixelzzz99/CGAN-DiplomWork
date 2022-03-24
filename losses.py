b92117f3e96b8040f2f5d37103879437be64e6e1
Sherzik <sherzik99@gmail.com>
2022-03-24 11:34:58 +0500

    Added whole generator

--- losses.py
+++ losses.py
@@ -1,4 +1,18 @@
 from keras import losses
 
+
 def custom_loss(y_true, y_pred):
-    cosine
+    # return cosine_distance + MAE
+    cosine = losses.cosine_proximity(y_true, y_pred)
+    mle = losses.mean_absolute_error(y_true, y_pred)
+    loss = (cosine) + mle
+    return loss
+
+
+def custom_loss_2(y_true, y_pred):
+    # scaled cosine_distance with MES + MAE
+    cosine = losses.cosine_proximity(y_true, y_pred)
+    mse = losses.mean_squared_error(y_true, y_pred)
+    mle = losses.mean_absolute_error(y_true, y_pred)
+    loss = (1 + cosine) * mse + mle
+    return loss
